2d82b127eedc5c01dbcb312818499732387028a4
BenedictoGeraldo <geraldodawa@gmail.com>
2026-05-14 23:00:40 +0700

    init project

---
+++ src/test/java/com/bene/mahasiswacrudspring/MahasiswaCrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bene.mahasiswacrudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahasiswaCrudSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
